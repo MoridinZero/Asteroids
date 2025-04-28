7aa4053c45d4204ad15951188d028f14dc26dfed
MoridinZero <logan.garey@gmail.com>
2025-04-28 13:50:05 -0700

    fps changes

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     infinte_number = 1
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while infinte_number != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                infinte_number = 1
+                infinte_number = 0
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
