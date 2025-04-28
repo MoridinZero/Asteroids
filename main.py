23546158272b55d17e681635c6205fa2145621af
MoridinZero <logan.garey@gmail.com>
2025-04-28 12:49:12 -0700

    added to the main.py file

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    infinte_number = 1
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while infinte_number != 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                infinte_number = 1
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
